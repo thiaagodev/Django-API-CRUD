00525d5b422820dd7f637e74a906cddc3e79d42e
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-26 17:53:30 -0300

    Adicionando books na pagina de super admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from books.models import Books
 
 # Register your models here.
+
+class Book(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_link = ('id', 'title')
+    search_fields = ('id', 'title',)
+    list_per_page = 10
+
+admin.site.register(Books, Book)
--- books/models.py
+++ books/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from uuid import uuid4
 
 # Create your models here.
 
 
 class Books(models.Model):
-    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     realease_year = models.IntegerField()
@@ -13,3 +11,6 @@ class Books(models.Model):
     pages = models.IntegerField()
     publisher_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
