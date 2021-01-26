1efa744eec1afbccadd15a6cbf02c189762a6749
Thiago Ferreira <thiaago.dev@gmail.com>
2021-01-26 18:12:12 -0300

    Ajustes

--- books/views.py
+++ books/views.py
@@ -8,8 +8,8 @@ class BooksViewSet(viewsets.ModelViewSet):
     queryset = models.Books.objects.all()
 
 class OrderBooksByDate(generics.ListAPIView):
+    serializer_class = serializers.OrderBooksByDate
     def get_queryset(self):
         queryset = models.Books.objects.order_by('realease_year')
         return queryset
-    serializer_class = serializers.OrderBooksByDate
 
